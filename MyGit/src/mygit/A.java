33a34ecaa510202c3b915e9e549783dce9379ccd
user <user@DESKTOP-JNDICN1>
2023-12-23 19:44:58 +0900

    Change in master branch

--- MyGit/src/mygit/A.java
+++ MyGit/src/mygit/A.java
@@ -4,6 +4,7 @@ public class A {
 	public static void main(String[] args) {
 		System.out.println("i am A!");
 		System.out.println("i am 2nd A!");
+		System.out.println("My changed in master branch");
 	}
 
 }
---
+++ MyGit/src/mygit/YourClass.java
@@ -0,0 +1,9 @@
+package mygit;
+
+public class YourClass {
+	public void SayHello(String Hi) {
+		System.out.println("Hello + Hi");
+		System.out.println("Your change in master branch");
+	}
+
+}
