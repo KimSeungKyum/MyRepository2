a4f937a54f2ecb24dccf18081f40903e1b661338
user <user@DESKTOP-JNDICN1>
2023-12-23 19:52:26 +0900

    Change java

--- MyGit/src/mygit/B.java
+++ MyGit/src/mygit/B.java
@@ -3,6 +3,7 @@ package mygit;
 public class B {
 	public static void main(String[] args) {
 		System.out.println("i am A!");
+		System.out.println("My changed B java in master branch");
 	}
 	
 }
--- MyGit/src/mygit/YourClass.java
+++ MyGit/src/mygit/YourClass.java
@@ -3,7 +3,7 @@ package mygit;
 public class YourClass {
 	public void SayHello(String Hi) {
 		System.out.println("Hello + Hi");
-		System.out.println("Your change in master branch");
+		System.out.println("Your change B java in master branch");
 	}
 
 }
