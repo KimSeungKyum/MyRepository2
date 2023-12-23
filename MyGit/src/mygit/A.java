2bb65a3b12e962c463924fcdf0cf35a8c7255fb5
user <user@DESKTOP-JNDICN1>
2023-12-23 19:43:15 +0900

    First commit in dev bracnch.

--- MyGit/src/mygit/A.java
+++ MyGit/src/mygit/A.java
@@ -4,6 +4,8 @@ public class A {
 	public static void main(String[] args) {
 		System.out.println("i am A!");
 		System.out.println("i am 2nd A!");
+		YourClass your = new YourClass();
+		your.SayHello("Hi");
 	}
 
 }
