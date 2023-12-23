86cf4ae76706ef127ad83412e9e4cbdfad9daf26
user <user@DESKTOP-JNDICN1>
2023-12-23 19:56:43 +0900

    Change in master branch

--- MyGit/src/mygit/C.java
+++ MyGit/src/mygit/C.java
@@ -3,6 +3,8 @@ package mygit;
 public class C {
 	public static void main(String[] args) {
 		System.out.println("i am A!");
+		System.out.println("My changed  java in master branch");
+		System.out.println("Change C java");
 	}
 	
 }
